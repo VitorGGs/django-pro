5e820df8fcb957142e27391dffc29cef47bc1a7b
vitor <vitorgabriel.unb@gmail.com>
2020-09-01 23:52:29 -0300

    consertado o bug 400 bad request no heroku

--- pypro/settings.py
+++ pypro/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['https://vitor-djangopro.herokuapp.com/']
+ALLOWED_HOSTS = ['*']
 
 AUTH_USER_MODEL = 'base.User'
 
